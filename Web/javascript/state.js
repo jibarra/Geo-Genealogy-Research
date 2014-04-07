acfd6cd2e7235bbb9dcf89ae4b84535c42e4c818
Jose Ibarra <jose.ibarra.ji@gmail.com>
2014-04-07 12:43:41 -0700

    Linted the state.js file.

--- Web/javascript/state.js
+++ Web/javascript/state.js
@@ -1,3 +1,12 @@
+/**
+ * @author Feng Wang
+ * @author Jose Ibarra, Jose.Ibarra@asu.edu, Arizona State University
+ * Modified on September 10, 2013 and after into 2014
+
+  Linted on April 7, 2014 using JSHint (https://github.com/jshint/jshint/blob/master/examples/.jshintrc)
+
+ */
+
 var state = {
 	dateFormat : "%d/%m/%z %H:%i",
 	//These are the variables for the maps' center lat and lon
@@ -6,16 +15,16 @@ var state = {
 	//This is the variable for the maps' zoom level
 	//Lower numbers show more of the world (more zoomed out)
 	zoomLevel : 4,
-	surnameOverlayImage : 'images/blank.png',
-	forenameOverlayImage : 'images/blank.png',
-	combinedOverlayImage : 'images/blank.png',
-	surnameCaptureImage : 'images/blank.png',
-	forenameCaptureImage : 'images/blank.png',
-	combineCaptureImage : 'images/blank.png',
+	surnameOverlayImage : "images/blank.png",
+	forenameOverlayImage : "images/blank.png",
+	combinedOverlayImage : "images/blank.png",
+	surnameCaptureImage : "images/blank.png",
+	forenameCaptureImage : "images/blank.png",
+	combineCaptureImage : "images/blank.png",
 	surnameLoaded : false,
 	forenameLoaded : false,
-	kdeSurname : '',
-	kdeForename: '',
+	kdeSurname : "",
+	kdeForename: "",
 	surnameData : null,
 	forenameData : null,
 	kdeOpacity : 0.4,
@@ -23,10 +32,10 @@ var state = {
 	kdeMaxBandwidth : 120000,
 	showMarker : false,
 	markers : [],
-	kdeColor : 'quantile',
+	kdeColor : "quantile",
 	//This is the location for the web services. If this is set up
-	//using your own computer as the server, the address should be something 'like http://localhost:8080/nameservice/'
-	// serviceBase: 'http://localhost:8080/nameservice/',
-	serviceBase: 'http://ec2-54-201-211-102.us-west-2.compute.amazonaws.com:8080/surnameservice/',
-	incomeServiceBase : '',
-}
+	//using your own computer as the server, the address should be something "like http://localhost:8080/nameservice/"
+	// serviceBase: "http://localhost:8080/nameservice/",
+	serviceBase: "http://ec2-54-201-211-102.us-west-2.compute.amazonaws.com:8080/surnameservice/",
+	incomeServiceBase : "",
+};
