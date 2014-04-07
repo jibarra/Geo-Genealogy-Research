abf06d4f866c05b2358b0909b1b685d64911e1af
Jose Ibarra <jose.ibarra.ji@gmail.com>
2014-04-07 12:41:51 -0700

    Linted the query.js file.

--- Web/javascript/query.js
+++ Web/javascript/query.js
@@ -2,10 +2,14 @@
  * @author Feng Wang
  * @author Jose Ibarra, Jose.Ibarra@asu.edu, Arizona State University
  * Modified on September 10, 2013 and after into 2014
+
+  Linted on April 7, 2014 using JSHint (https://github.com/jshint/jshint/blob/master/examples/.jshintrc)
+
  */
 var disableQueries = false;
 var isCaptured = false;
 function kdeQuery(nameType, mapType) {
+	"use strict";
 	if(disableQueries)
 		return;
 	//resetView();
@@ -16,11 +20,11 @@ function kdeQuery(nameType, mapType) {
 	var height = null;
 	var queryData = null;
 	if(nameType == "surname"){
-		if (typeof (kde.overlay) == 'undefined' || kde.overlay == null) {
+		if (typeof (kde.overlay) == "undefined" || kde.overlay === null) {
 			return;
 		}
-		var width = document.getElementById("surnamemap").clientWidth;
-		var height = document.getElementById("surnamemap").clientHeight;
+		width = document.getElementById("surnamemap").clientWidth;
+		height = document.getElementById("surnamemap").clientHeight;
 		boundsOnEarth = kde.map.getBounds();
 
 		queryData = {
@@ -37,11 +41,11 @@ function kdeQuery(nameType, mapType) {
 		};
 	}
 	else if(nameType == "forename"){
-		if (typeof (forenamekde.overlay) == 'undefined' || forenamekde.overlay == null) {
+		if (typeof (forenamekde.overlay) == "undefined" || forenamekde.overlay === null) {
 			return;
 		}
-		var width = document.getElementById("forenamemap").clientWidth;
-		var height = document.getElementById("forenamemap").clientHeight;
+		width = document.getElementById("forenamemap").clientWidth;
+		height = document.getElementById("forenamemap").clientHeight;
 		boundsOnEarth = forenamekde.map.getBounds();
 
 		queryData = {
@@ -61,63 +65,68 @@ function kdeQuery(nameType, mapType) {
 }
 
 function resetView(){
-	$('#captureButton').hide();
-	$('#capturedimagelink').hide();
-	$('#surnameMedianIncomeText').hide();
-	$('#surnameMeanIncomeText').hide();
+	"use strict";
+	$("#captureButton").hide();
+	$("#capturedimagelink").hide();
+	$("#surnameMedianIncomeText").hide();
+	$("#surnameMeanIncomeText").hide();
 	var curURL = document.URL;
 	var completeURL = "https://twitter.com/share?url=" + curURL + "&text=I found my surname density on " + curURL;
-	$('#twitter').attr('href', completeURL);
+	$("#twitter").attr("href", completeURL);
 	if(isCaptured){
-		$('#footer').css('height', '10%');
-		$('#centercontainer').css('height', '90%');
-		$('#surnamemap').css('height', '100%');
-		var body=document.getElementsByTagName('body')[0];
-		body.style.height='100%';
+		$("#footer").css("height", "10%");
+		$("#centercontainer").css("height", "90%");
+		$("#surnamemap").css("height", "100%");
+		var body=document.getElementsByTagName("body")[0];
+		body.style.height="100%";
 		disableQueries = true;
 		kde.map.setCenter(new google.maps.LatLng(state.centerLat, state.centerLon));
-		setTimeout(function(){ 	disableQueries = false }, 1000);
+		setTimeout(function(){
+			disableQueries = false;
+		}, 1000);
 		isCaptured=false;
 	}
 }
 
 function kdeQueryImp(configuredData, nameType, mapType) {
+	"use strict";
+	var scriptLocation;
 	if(nameType == "surname"){
-		var scriptLocation = state.serviceBase + 'services/querySurname' + mapType +'?callback=?';
-		$('#loadingsurname').show();
+		scriptLocation = state.serviceBase + "services/querySurname" + mapType +"?callback=?";
+		$("#loadingsurname").show();
 	}
 	else if(nameType == "forename"){
-		var scriptLocation = state.serviceBase + 'services/queryForename?callback=?';
-		$('#loadingforename').show();
+		scriptLocation = state.serviceBase + "services/queryForename?callback=?";
+		$("#loadingforename").show();
 	}
 
 	$.ajax(scriptLocation, {
 		data : configuredData,
-		dataType : 'jsonp',
+		dataType : "jsonp",
 		global: true,
 
 		error : function(jqXHR, textStatus, errorThrown) {
 			console.log(textStatus);
 			console.log(jqXHR);
 			console.log(errorThrown);
-			$('#loadingsurname').hide();
-			alert('Error');
+			$("#loadingsurname").hide();
+			alert("Error");
 		},
 		success : function(data, textStatus, jqXHR) {
 			updateKDEMap(data, nameType, mapType);
 			// updateIncome(data, nameType);
 			if(nameType == "surname"){
-				$('#loadingsurname').hide();
+				$("#loadingsurname").hide();
 				state.surnameLoaded = true;
 			}
 			else if (nameType == "forename"){
-				$('#loadingforename').hide();
+				$("#loadingforename").hide();
 				state.forenameLoaded = true;
 			}
 
-			if(state.surnameOverlayImage != null && state.forenameOverlayImage != null
-					&& state.surnameOverlayImage != 'blank.png' && state.forenameOverlayImage != 'blank.png'
-					&& state.surnameLoaded == true && state.forenameLoaded == true){
+			if(state.surnameOverlayImage !== null && state.forenameOverlayImage !== null &&
+					state.surnameOverlayImage != "blank.png" && state.forenameOverlayImage != "blank.png" &&
+					state.surnameLoaded === true && state.forenameLoaded === true){
 				$( "#tabs" ).tabs("enable", 2);
 			}
 		}
@@ -125,89 +134,93 @@ function kdeQueryImp(configuredData, nameType, mapType) {
 }
 
 function updateKDEMap(data, nameType, mapType) {
+	"use strict";
 	if(nameType == "surname"){
-		if(data.image == null || data.image == 'blank.png'){
+		if(data.image === null || data.image == "blank.png"){
 			alert("The image and income data for this name could not be loaded. The name's " +
-		      	"population may be too small or the name may not exist in the database.");
+				"population may be too small or the name may not exist in the database.");
 		}
-		state.surnameOverlayImage = state.serviceBase + 'resources/' + data.image;
+		state.surnameOverlayImage = state.serviceBase + "resources/" + data.image;
 		
 		// refresh
-		if (kde.overlay != null) {
+		if (kde.overlay !== null) {
 			kde.overlay.setMap(null);
 			delete kde.overlay;
 		}
 		kde.overlay = new KDEOverlay(kde.map, "surname");
-		$('#captureButton').show();
+		$("#captureButton").show();
 	}
 	else if(nameType == "forename"){
 		state.forenameOverlayImage = state.serviceBase + "image/kdecacheforename/" + data.image;
 		// refresh
-		if (forenamekde.overlay != null) {
+		if (forenamekde.overlay !== null) {
 			forenamekde.overlay.setMap(null);
 			delete forenamekde.overlay;
 		}
 		forenamekde.overlay = new KDEOverlay(forenamekde.map, "forename");
-		$('#forenameCaptureButton').show();
+		$("#forenameCaptureButton").show();
 	}
 	else if(nameType == "combined"){
 		state.combinedOverlayImage = data.image;
 		// refresh
-		if (kde3.overlay != null) {
+		if (kde3.overlay !== null) {
 			kde3.overlay.setMap(null);
 			delete kde3.overlay;
 		}
 		kde3.overlay = new KDEOverlay(kde3.map, "combined");
 	}
 
-	if(state.surnameOverlayImage != 'blank.png' && state.forenameOverlayImage != 'blank.png')
-		$('#combine_submit').show();
+	if(state.surnameOverlayImage != "blank.png" && state.forenameOverlayImage != "blank.png")
+		$("#combine_submit").show();
 	else
-		$('#combine_submit').hide();
+		$("#combine_submit").hide();
 	positionSocialMedia();
 }
 
 //Formats money for easier reading
 Number.prototype.formatMoney = function(c, d, t){
-var n = this, 
-    c = isNaN(c = Math.abs(c)) ? 2 : c, 
-    //if d is undefined, make it the default decimal value (.)
-    d = d == undefined ? "." : d, 
-    //if t is undefined, make it the default number separator(,)
-    t = t == undefined ? "," : t, 
-    //Add a - if the number is negative
-    s = n < 0 ? "-" : "", 
-    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
-    j = (j = i.length) > 3 ? j % 3 : 0;
-   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+	"use strict";
+	var n = this,
+	c = isNaN(c = Math.abs(c)) ? 2 : c,
+	//if d is undefined, make it the default decimal value (.)
+	d = d === undefined ? "." : d,
+	//if t is undefined, make it the default number separator(,)
+	t = t === undefined ? "," : t,
+	//Add a - if the number is negative
+	s = n < 0 ? "-" : "",
+	i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+	j = (j = i.length) > 3 ? j % 3 : 0;
+	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
  };
 
 function updateIncome(data, nameType){
+	"use strict";
 	var mean = data.averageMean.formatMoney(2);
 	var median = data.averageMedian.formatMoney(2);
 	if(nameType=="surname"){
-		$('#surnameMeanIncomeText').text("Average Mean Income: $" + mean);
-		$('#surnameMedianIncomeText').text("Average Median Income: $" + median);
-		$('#surnameMeanIncomeText').show();
-		$('#surnameMedianIncomeText').show();
+		$("#surnameMeanIncomeText").text("Average Mean Income: $" + mean);
+		$("#surnameMedianIncomeText").text("Average Median Income: $" + median);
+		$("#surnameMeanIncomeText").show();
+		$("#surnameMedianIncomeText").show();
 	}
 	else if (nameType=="forename"){
-		$('#forenameMeanIncomeText').text("Average Mean Income: $" + mean);
-		$('#forenameMedianIncomeText').text("Average Median Income: $" + median);
-		$('#forenameMeanIncomeText').show();
-		$('#forenameMedianIncomeText').show();
+		$("#forenameMeanIncomeText").text("Average Mean Income: $" + mean);
+		$("#forenameMedianIncomeText").text("Average Median Income: $" + median);
+		$("#forenameMeanIncomeText").show();
+		$("#forenameMedianIncomeText").show();
 	}
 
 }
 
 function captureUpload(nameType) {
-	if (typeof (kde.overlay) == 'undefined' || kde.overlay == null) {
+	"use strict";
+	if (typeof (kde.overlay) == "undefined" || kde.overlay === null) {
 		return;
 	}
 
 	// the coordinates for the bounds
 	var boundsOnEarth = null;
-	var name = null;;
+	var name = null;
 	var imageLoc = null;
 	var zoomLevel = null;
 	if(nameType=="surname"){
@@ -243,27 +256,28 @@ function captureUpload(nameType) {
 }
 
 function captureUploadImp(configuredData, nameType) {
+	"use strict";
 	if(nameType=="surname"){
-		scriptLocation = state.serviceBase + 'services/surnamebuildmap?callback=?';
-		$('#capturedimagelink').hide();
-		$('#loadingsurname').show();
+		scriptLocation = state.serviceBase + "services/surnamebuildmap?callback=?";
+		$("#capturedimagelink").hide();
+		$("#loadingsurname").show();
 	}
 	else if(nameType=="forename"){
-		scriptLocation = state.serviceBase + 'services/forenamebuildmap?callback=?';
-		$('#forenameCapturedimagelink').hide();
-		$('#loadingforename').show();
+		scriptLocation = state.serviceBase + "services/forenamebuildmap?callback=?";
+		$("#forenameCapturedimagelink").hide();
+		$("#loadingforename").show();
 	}
 
 	$.ajax(scriptLocation, {
 		data : configuredData,
-		dataType : 'jsonp',
+		dataType : "jsonp",
 		global: true,
 
 		error : function(jqXHR, textStatus, errorThrown) {
 			console.log(textStatus);
 			console.log(jqXHR);
 			console.log(errorThrown);
-			alert('Error');
+			alert("Error");
 		},
 		success : function(data, textStatus, jqXHR) {
 			console.log(data.filename);
@@ -275,29 +289,30 @@ function captureUploadImp(configuredData, nameType) {
 			if(nameType=="surname"){
 				state.surnameCaptureImage = completeURL;
 				center = kde.map.getBounds().getCenter();
-				$('#loadingsurname').hide();
-				$('#captureButton').hide();
-				$('#capturedimagelink').attr('href', completeURL);
-				$('#capturedimagelink').show();
+				$("#loadingsurname").hide();
+				$("#captureButton").hide();
+				$("#capturedimagelink").attr("href", completeURL);
+				$("#capturedimagelink").show();
 			}
 			else if(nameType=="forename"){
 				state.forenameCaptureImage = completeURL;
 				center = forenamekde.map.getBounds().getCenter();
-				$('#loadingforename').hide();
-				$('#forenameCaptureButton').hide();
-				$('#forenameCapturedimagelink').attr('href', completeURL);
-				$('#forenameCapturedimagelink').show();
+				$("#loadingforename").hide();
+				$("#forenameCaptureButton").hide();
+				$("#forenameCapturedimagelink").attr("href", completeURL);
+				$("#forenameCapturedimagelink").show();
 			}
 		}
 	});
 }
 
 function kdeCombineQuery() {
-	if(state.surnameOverlayImage == null || state.forenameOverlayImage == null
-			|| state.surnameOverlayImage == 'blank.png' || state.forenameOverlayImage == 'blank.png'){
+	"use strict";
+	if(state.surnameOverlayImage === null || state.forenameOverlayImage === null ||
+			state.surnameOverlayImage == "blank.png" || state.forenameOverlayImage == "blank.png"){
 		return;
 	}
-	if (typeof (kde3.overlay) == 'undefined' || kde3.overlay == null) {
+	if (typeof (kde3.overlay) == "undefined" || kde3.overlay === null) {
 		return;
 	}
 
@@ -310,20 +325,19 @@ function kdeCombineQuery() {
 }
 
 function kdeCombineQueryImp(configuredData){
-	console.log(configuredData);
-
-	var scriptLocation = state.serviceBase + 'services/queryCombineNames?callback=?';
+	"use strict";
+	var scriptLocation = state.serviceBase + "services/queryCombineNames?callback=?";
 
 	$.ajax(scriptLocation, {
 		data : configuredData,
-		dataType : 'jsonp',
+		dataType : "jsonp",
 		global: true,
 
 		error : function(jqXHR, textStatus, errorThrown) {
 			console.log(textStatus);
 			console.log(jqXHR);
 			console.log(errorThrown);
-			alert('Error');
+			alert("Error");
 		},
 		success : function(data, textStatus, jqXHR) {
 			updateKDEMap(data, "combined");
@@ -334,12 +348,13 @@ function kdeCombineQueryImp(configuredData){
 }
 
 function combineMapsCapture(){
-	if (typeof (kde.overlay) == 'undefined' || kde.overlay == null) 
+	"use strict";
+	if (typeof (kde.overlay) == "undefined" || kde.overlay === null)
 		return;
-	if (typeof (forenamekde.overlay) == 'undefined' || forenamekde.overlay == null)
+	if (typeof (forenamekde.overlay) == "undefined" || forenamekde.overlay === null)
 		return;
 	var boundsOnEarth1 = kde.map.getBounds();
-	var boundsOnEarth2 = forenamekde.map.getBounds()
+	var boundsOnEarth2 = forenamekde.map.getBounds();
 	var latsw1 = boundsOnEarth1.getSouthWest().lat();
 	var latsw2 = boundsOnEarth2.getSouthWest().lat();
 	var lngsw1 = boundsOnEarth1.getSouthWest().lng();
@@ -369,30 +384,31 @@ function combineMapsCapture(){
 }
 
 function combineMapsCaptureImp(configuredData){
-	scriptLocation = state.serviceBase + 'services/combinebuildmap?callback=?';
-	$('#loadingCombine').show();
+	"use strict";
+	scriptLocation = state.serviceBase + "services/combinebuildmap?callback=?";
+	$("#loadingCombine").show();
 	positionSocialMedia();
 
 	$.ajax(scriptLocation, {
 		data : configuredData,
-		dataType : 'jsonp',
+		dataType : "jsonp",
 		global: true,
 
 		error : function(jqXHR, textStatus, errorThrown) {
 			console.log(textStatus);
 			console.log(jqXHR);
 			console.log(errorThrown);
-			alert('Error');
-			$('#loadingCombine').hide()
+			alert("Error");
+			$("#loadingCombine").hide();
 			positionSocialMedia();
 		},
 		success : function(data, textStatus, jqXHR) {
 			var completeURL = state.httpServiceBase + data.url;
 			state.combineCaptureImage = completeURL;
-			$('#loadingCombine').hide()
-			$('#combine_submit').hide();
-			$('#combineCapturedImageLink').attr('href', completeURL);
-			$('#combineCapturedImageLink').show();
+			$("#loadingCombine").hide();
+			$("#combine_submit").hide();
+			$("#combineCapturedImageLink").attr("href", completeURL);
+			$("#combineCapturedImageLink").show();
 			positionSocialMedia();
 		}
 	});
